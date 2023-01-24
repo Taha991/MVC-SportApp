fd764c836c34479aef0fef1df49adbd5d91a7cf0
Taha991 <tahamohamedman@gmail.com>
2023-01-24 13:16:49 +0200

    IP Location + Edit Home Page

--- SportWebApp/Controllers/HomeController.cs
+++ SportWebApp/Controllers/HomeController.cs
@@ -1,21 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SportWebApp.Helpers;
+using SportWebApp.Interfaces;
 using SportWebApp.Models;
+using SportWebApp.ViewModels;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace SportWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , IClubRepository clubRepository)
         {
             _logger = logger;
+            _clubRepository = clubRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IpInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "ipinfo.io/102.47.33.96?token=ce98eb06e6dc59";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IpInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if(homeViewModel.City != null)
+                {
+                    homeViewModel.Clubs = await _clubRepository.GetClubByCity(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception ex)
+            {
+                homeViewModel.Clubs = null;
+            }
+
+            return View(homeViewModel);
+          
         }
 
         public IActionResult Privacy()
---
+++ SportWebApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SportWebApp.Models;
+
+namespace SportWebApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Club>? Clubs { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        //public HomeUserCreateViewModel Register { get; set; } = new HomeUserCreateViewModel();
+    }
+}
